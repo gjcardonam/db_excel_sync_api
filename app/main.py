fdd640c9e80e91a33e149a04deede6c380ba2a7c
Gabriel J Cardona <gjcardonam@gmail.com>
2025-07-16 15:16:02 -0500

    fix: configuracion cors

--- app/main.py
+++ app/main.py
@@ -1,14 +1,24 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.v1.endpoints.upload import router as upload_router
 from app.api.v1.endpoints.health import router as health_router
-from app.api.v1.endpoints.web import router as web_router  # 👈 nuevo router
+from app.api.v1.endpoints.web import router as web_router
 
 app = FastAPI(title="Excel Upload API")
 
-# API routes
+# Rutas de API
 app.include_router(upload_router, prefix="/api/v1")
 app.include_router(health_router, prefix="/api/v1")
 
-# Web form routes (sin prefijo para que / sea la raíz)
+# Ruta para formulario web
 app.include_router(web_router)
+
+# Middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
